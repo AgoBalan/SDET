76d9aa43433756b704a7d3f5571e3b5caf658e11
Ago Ben <jehovabalan@gmail.com>
2025-09-15 16:23:26 -0400

    dataprovider

--- src/test/java/com/example/test/GridServer1Test.java
+++ src/test/java/com/example/test/GridServer1Test.java
@@ -1,18 +1,26 @@
 package com.example.test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.checkerframework.checker.units.qual.s;
 import org.openqa.selenium.By;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class GridServer1Test extends BaseTest {
     
-    @Test(dataProvider = "customerData")
+    @Test(dataProvider = "excelData")
     public void testAsiaCustomers(String region) throws Exception {
         
         // Navigate to your web application
         driver.get("https://www.google.com");
-        System.out.println("Title: " + driver.getTitle());
-        driver.findElement(By.partialLinkText("type")).sendKeys(region);
+     
+        driver.findElement(By.xpath("//textarea[@title='Search']")).sendKeys(region);
+           System.out.println("Title: " + driver.getTitle());
         driver.quit();
         try{
         Thread.sleep(1000);
@@ -26,5 +34,22 @@ public class GridServer1Test extends BaseTest {
             {"Europe"},
             {"America"}
         };
+    }
+
+      @DataProvider(name = "excelData")
+    public Object[][] excelDataProvider() throws IOException {
+     FileInputStream file = new FileInputStream("ExcelForDataProvider.xlsx");
+     try (XSSFWorkbook workbook = new XSSFWorkbook(file)) {
+        XSSFSheet sheet = workbook.getSheetAt(0);
+        int rowCount = sheet.getPhysicalNumberOfRows();
+        int colCount = sheet.getRow(0).getLastCellNum();
+        Object[][] data = new Object[rowCount-1][colCount];
+        for(int i=1; i<rowCount; i++) {
+            for(int j=0; j<colCount; j++) {
+                data[i-1][j] = sheet.getRow(i).getCell(j).toString();
+            }
+        }
+        return data;
+    }
     }
 }
